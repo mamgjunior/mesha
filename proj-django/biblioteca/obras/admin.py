4b74edd5ebf19b2716f8f68f3f94cb0218dc3beb
mamgjunior <mamgjunior@hotmail.com>
2021-10-11 23:47:29 -0300

    Criando a classe model, iniciando as migrações, alterando o admin e testando a base de dados

--- proj-django/biblioteca/obras/admin.py
+++ proj-django/biblioteca/obras/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Obras
 
 # Register your models here.
+
+@admin.register(Obras)
+class ObrasAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editora', 'autor', 'criado_em')
+    list_filter = ('autor', 'criado_em')
+    search_fields = ('titulo', 'autor')
+    ordering = ('-criado_em', 'autor')
--- proj-django/biblioteca/obras/models.py
+++ proj-django/biblioteca/obras/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Obras(models.Model):
+    titulo = models.CharField(max_length=200, verbose_name='Titulo')
+    editora = models.CharField(max_length=150, verbose_name='Editora')
+    autor = models.CharField(max_length=200, verbose_name='Autor')
+    foto = models.ImageField(upload_to='obras/img', null=True, blank=True, verbose_name='Imagem')
+    criado_em = models.DateTimeField(auto_now=True, verbose_name='Criado em')
+
+    def __str__(self) -> str:
+        return self.titulo
